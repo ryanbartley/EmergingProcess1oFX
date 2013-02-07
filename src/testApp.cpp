732d597b302c6a60afd91e1af1f014304cdf0064
Ryan Bartley <ryanbartley@gmail.com>
2013-02-07 09:26:50 -0500

    Finished implementing the pixel and frame combination for the video player.

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -4,92 +4,51 @@
 void testApp::setup(){
     ofBackground(255, 255, 255);
     
-    frameByFrame = false;
+    
     
     fingerMovie.loadMovie("movies/fingers.mov");
     fingerMovie.play();
-    fingerMovie.setPaused(true);
-    numFrames = fingerMovie.getTotalNumFrames();
-
+    //fingerMovie.setPaused(true);
+    
+    h = fingerMovie.getHeight();
+    w = fingerMovie.getWidth();
+    frame = 0;
+    totalFrames = fingerMovie.getTotalNumFrames();
+    
+    
 }
 
 //--------------------------------------------------------------
 void testApp::update(){
 
-    //fingerMovie.update();
+    fingerMovie.update();
     
 }
 
 //--------------------------------------------------------------
 void testApp::draw(){
-    int currentFrame = 0;
-    unsigned char * pixels = fingerMovie.getPixels();
-    int nChannels = fingerMovie.getPixelsRef().getNumChannels();
-    int width = fingerMovie.getWidth();
-    int height = fingerMovie.getHeight();
-
-    for (int i = 1; i < width || currentFrame != numFrames ; ++i) {
-        for (int j = 1; j < height || currentFrame != numFrames; ++j) {
+    
+    for (int y=0; y<h; y+=10) {
+        for (int x=0; x<w; x+=10) {
             
-            unsigned char r = pixels[(j * width + i)*nChannels];
-            unsigned char g = pixels[(j * width + i)*nChannels];
-            unsigned char b = pixels[(j * width + i)*nChannels];
+            //get pixel color
+            int i = (y*w+x)*3;
+            int r = fingerMovie.getPixels()[i+0];
+            int g = fingerMovie.getPixels()[i+1];
+            int b = fingerMovie.getPixels()[i+2];
             
             ofSetColor(r, g, b);
-            ofRect(i, j, 1, 1);
-            if (currentFrame != numFrames) {
-                fingerMovie.nextFrame();
-                ++currentFrame;
+            
+            ofRect(x, y, 10, 10);
+            
+            if (frame <= totalFrames) {
+                fingerMovie.setFrame(++frame);
             } else {
-                fingerMovie.firstFrame();
+                frame = 0;
+                fingerMovie.setFrame(frame);
             }
             
         }
     }
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
--- src/testApp.h
+++ src/testApp.h
@@ -8,19 +8,10 @@ class testApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
-
-		void keyPressed  (int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
     
-        ofVideoPlayer   fingerMovie;
-        bool            frameByFrame;
-		long            numFrames;
+        ofVideoPlayer fingerMovie;
+        float           w, h;
+        int             frame, totalFrames;
+    
     
 };
