c8e47ce672de6d97951198f80fa65fd97121eb87
Ryan Bartley <ryanbartley@gmail.com>
2013-02-07 09:38:03 -0500

    Commenting the program

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -10,9 +10,14 @@ void testApp::setup(){
     fingerMovie.play();
     //fingerMovie.setPaused(true);
     
+    //set the bounds for the 'for' loop
     h = fingerMovie.getHeight();
     w = fingerMovie.getWidth();
+    
+    //null out the frame variable
     frame = 0;
+    
+    //and then set the bounds for the loop of the film
     totalFrames = fingerMovie.getTotalNumFrames();
     
     
@@ -20,7 +25,8 @@ void testApp::setup(){
 
 //--------------------------------------------------------------
 void testApp::update(){
-
+    
+    //I don't know that I need this anymore.
     fingerMovie.update();
     
 }
@@ -37,10 +43,14 @@ void testApp::draw(){
             int g = fingerMovie.getPixels()[i+1];
             int b = fingerMovie.getPixels()[i+2];
             
+            //set drawing color to the pixel values
             ofSetColor(r, g, b);
             
+            //draw a rectangle with that pixel color
             ofRect(x, y, 10, 10);
             
+            //this sets the loop of the video so that 
+            //we can get a pixel from each frame
             if (frame <= totalFrames) {
                 fingerMovie.setFrame(++frame);
             } else {
